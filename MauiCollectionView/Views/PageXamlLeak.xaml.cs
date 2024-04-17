547b7fa6774af4cefddcccab66b7111b67cec07e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-04-17 14:12:43 -0500

    Change to show gesture leak

--- MauiCollectionView/Views/PageXamlLeak.xaml.cs
+++ MauiCollectionView/Views/PageXamlLeak.xaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MauiCollectionView.Views;
 
@@ -9,11 +10,23 @@ public partial class PageXamlLeak : ContentPage
         InitializeComponent();
 
         var collectionView = (CollectionView)Content;
-        collectionView.ItemsSource = new ObservableCollection<string> { "1", "2", "3" };
+        collectionView.ItemsSource = new MyModel[]
+        {
+            "1", "2", "3"
+        };
     }
 
     ~PageXamlLeak()
     {
         Console.WriteLine("~PageXamlLeak() called");
     }
+}
+
+class MyModel
+{
+    public static implicit operator MyModel(string value) => new() { Name = value };
+
+    public string Name { get; set; } = "";
+
+    public ICommand Command { get; set; } = new Command(() => Console.WriteLine("Tapped!"));
 }
